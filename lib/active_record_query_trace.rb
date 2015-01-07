7b734209ddf0040ad337c3a69c1bb9f4481d5b49
kingston <kingston@kreatio.com>
2015-01-07 20:11:29 +0530

    changed

--- lib/active_record_query_trace.rb
+++ lib/active_record_query_trace.rb
@@ -19,11 +19,11 @@ module ActiveRecordQueryTrace
         ActiveRecordQueryTrace.lines = 5
         ActiveRecordQueryTrace.new_log = true
         ActiveRecordQueryTrace.daywise = false
-        @logname = ActiveRecordQueryTrace.daywise ? "query-log-on-#{Time.now.strftime('%d-%m-%Y')}"  : "query"
 
       end
 
       def sql(event)
+        logname = ActiveRecordQueryTrace.daywise ? "query-log-on-#{Time.now.strftime('%d-%m-%Y')}"  : "query"
         if ActiveRecordQueryTrace.enabled
           index = begin
             if ActiveRecordQueryTrace.lines == 0
@@ -34,7 +34,7 @@ module ActiveRecordQueryTrace
           end
 
          if ActiveRecordQueryTrace.new_log
-             new_logger = Logger.new("#{Rails.root}/log/#{@logname}.log")
+             new_logger = Logger.new("#{Rails.root}/log/#{logname}.log")
              logging_info=[]
              logging_info << (color('Query: ', BLUE, true) + color(event.payload[:sql],RED,true))
              logging_info << (color('Time: ', GREEN, true) + color(event.time.to_datetime.strftime('%a %b %d %Y %H:%M:%S %Z'),YELLOW,true))
